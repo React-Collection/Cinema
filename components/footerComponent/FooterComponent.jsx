664178e35f67965aa6aac9d200ca17d1f4c700a6
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-09-20 22:58:04 +0330

    Feat(components): footer component A component includes repetitive and reusable elements in a page or design. Components are built with flash functions. The components are completely built with React.

---
+++ components/footerComponent/FooterComponent.jsx
@@ -0,0 +1,6 @@
+import footerStyles from "./index.module.scss";
+
+const FooterComponent = () => {
+  return <h2 className={footerStyles.titles}>The End ...</h2>;
+};
+export default FooterComponent;
