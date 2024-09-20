b3051f7b990648f4b538c1ec1a2500babbd325c1
Tboompsh <145778331+Tboumpsh@users.noreply.github.com>
2024-09-20 23:00:31 +0330

    Feat(components): constants variable image This file contains all the constant variables in the project. This variable includes photos that can be easily changed.

---
+++ constants/constImages.jsx
@@ -0,0 +1,6 @@
+export const HERO_SECTION_BANNER = "/public/images/Ten Tips for a Successful Presentation http___nikkiwoodsmedia_com_ten-tips-for-a-successful-presentation_.jpg"
+
+export const LOGO = "/public/images/RPC-JP_Logo.png"
+
+export const BANNER_RTL = "/public/images/ffa73aeb7163db532cbbb15b9c22ea41.jpg"
+export const BANNER_LTR = "/public/images/Spotlight.jpg"
